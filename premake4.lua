fe314aec230466e7296dee1033980a81f2f98bdf
Lauri Makinen <lauri.sakari.makinen@gmail.com>
2013-03-30 15:00:14 +0200

    Init commit

---
+++ premake4.lua
@@ -0,0 +1,38 @@
+solution "ProjectIce"
+    configurations { "Debug", "Release" }
+	flags { "ExtraWarnings", "FatalWarnings" }
+
+    -- Platform-specific options
+    configuration { "gmake" }
+		--buildoptions { "-std=c99", "-pedantic" }
+
+	configuration { "windows" }
+		defines { "WINDOWS" }
+
+    configuration { "linux" }
+		defines { "LINUX" }
+        links { "m", "rt" }
+
+    -- Configuration-specific options
+    configuration "Debug"
+        defines { "DEBUG" }
+        flags   { "Symbols" }
+
+    configuration "Release"
+        defines { "NDEBUG","RELEASE_BUILD" }
+        flags   { "Optimize" }
+
+    -- Client project
+    project "game"
+        kind "ConsoleApp"
+        language "C++"
+        files { "src/**.h", "src/**.cpp" }
+
+        configuration "windows"
+            -- TODO: Remove hard-coded paths
+            libdirs     { "C:/codelibs/pdcurses/lib/" }
+            includedirs { "C:/codelibs/pdcurses/include/" }
+            links       { "pdcurses" }
+
+        configuration "linux"
+            links { "ncurses" }
