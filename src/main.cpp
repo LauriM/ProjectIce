1ea5427c9419b293370f877b4c094f3615e1590e
Lauri Makinen <lauri.sakari.makinen@gmail.com>
2013-03-31 16:42:30 +0300

    testing the protobuffs, seems to be working fine

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,30 @@ int main(){
 	world->update();
 	world->uninit();
 
-	//Lets test protobuffers
-	
+	/**
+	 * Protobuff testing
+	 */
 	tile test;
 	test.set_posx(15);
 	test.set_posy(10);
 	test.set_visual("#");
+	test.set_roomid(0);
 
 	std::fstream stream;
 	stream.open("gamesave", std::ios::out | std::ios::binary);
 
 	test.SerializeToOstream(&stream);
 
+	stream.close();
+
+	std::fstream input;
+	input.open("gamesave",std::ios::in | std::ios::binary);
+
+	tile blob;
+	blob.ParseFromIstream(&input);
+
+	input.close();
+
+	printf("Tile x: %i tile y: %i [%s]\n",blob.posx(),blob.posy(),blob.visual().c_str());
 	return 0;
 }
