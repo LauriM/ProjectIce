e0ee966d2c898a1d472cd74689a8eb2d993e696f
Bryan Abrams <bryan.abrams@ctac.nl>
2013-07-31 12:32:40 +0200

    Replaced ItemSystem with Inventory

--- src/engine/world/item/BaseItem.h
+++ src/engine/world/item/BaseItem.h
@@ -3,6 +3,7 @@
 #define __BASE_ITEM_H__
 
 #include "precompiled.h"
+#include "engine/player/Player.h"
 
 namespace engine {
 namespace world {
@@ -10,19 +11,62 @@ namespace item {
 
 	class BaseItem {
 	private:
-		// TODO lets discuss the properties an item should have
-		//      Also how to organize everything
+
 		String _name;
-		int _value;
+		String _description;
+		double _value;
+		double _weight;
+		int _quantity;
+
+	protected:
+
+		// TODO This class is abstract, don't let people make a BaseItem (later, testing atm)
+		//BaseItem();
 
 	public:
-		BaseItem();
 
 		String getName();
-		int getValue();
+		String getDescription();
+		double getValue();
+		double getWeight();
+		int getQuantity();
 
 		void setName(String name);
-		void setValue(int value); 
+		void setDescription(String description);
+		void setValue(double value);
+		void setWeight(double weight);
+		void setQuantity(int quantity);
+
+		/*
+		LATER Change these to virtual and remove their implementations, the implementation for now
+		is just to test
+		virtual void onDestroy( player::Player * pOwner ) = 0;
+		virtual void onPickup( player::Player * pOwner ) = 0;
+		virtual void onDrop( player::Player * pOwner ) = 0;
+		virtual void onInspect( player::Player * pOwner ) = 0;
+		virtual void onUse( player::Player * pOwner ) = 0;
+		*/
+
+		void onDestroy( player::Player * pOwner ) {
+			printf( "Player %s destroyed %s!", pOwner->getName().c_str(), _name.c_str() );
+		}
+
+		void onPickup( player::Player * pOwner ) {
+			printf( "Player %s picked up %s!", pOwner->getName().c_str(), _name.c_str() );
+		}
+
+		void onDrop( player::Player * pOwner ) {
+			printf( "Player %s dropped %s!", pOwner->getName().c_str(), _name.c_str() );
+		}
+
+		void onInspect( player::Player * pOwner ) {
+			printf( "Player %s inspected %s!", pOwner->getName().c_str(), _name.c_str() );
+		}
+
+		void onUse( player::Player * pOwner ) {
+			printf( "Player %s used %s!", pOwner->getName().c_str(), _name.c_str() );
+		}
+
 	};
 
 }
---
+++ src/engine/world/item/Inventory.h
@@ -0,0 +1,37 @@
+
+#ifndef __INVENTORY_H__
+#define __INVENTORY_H__
+
+#include "precompiled.h"
+#include <map>
+
+#include "engine/world/item/BaseItem.h"
+#include "engine/player/Player.h"
+
+namespace engine {
+namespace world {
+namespace item {
+
+	class Inventory {
+	private:
+
+		std::map<String,BaseItem*> _inventoryMap;
+		player::Player * _playerOwner;
+
+	public:
+
+		void init(player::Player * owner);
+
+		bool destroyItem(String name);
+		BaseItem * dropItem(String name);
+		bool useItem(String name);
+		bool insertItem(BaseItem * b);
+		bool inspectItem(String name);
+
+	};
+
+}
+}
+}
+
+#endif
