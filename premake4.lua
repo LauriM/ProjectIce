f4fe8ae6e71071405a52684f1bb4ba6aa63c5484
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2013-08-06 13:44:46 +0300

    Removing those damn spaces...

--- premake4.lua
+++ premake4.lua
@@ -1,19 +1,14 @@
 solution "ProjectIce"
-    configurations { "Debug", "Release" }
-
-    -- Platform-specific options
-    configuration { "gmake" }
-		--buildoptions { "-std=c99", "-pedantic" }
+	configurations { "Debug", "Release" }
 
 	configuration { "windows" }
 		defines { "WINDOWS" }
 
-    configuration { "linux" }
+	configuration { "linux" }
 		defines { "LINUX" }
 		links { "m", "rt" }
 
-    -- Configuration-specific options
-    configuration "Debug"
+	configuration "Debug"
 		defines { "DEBUG" }
 		flags   { "Symbols" }
 
@@ -21,21 +16,21 @@ solution "ProjectIce"
 		defines { "NDEBUG","RELEASE_BUILD" }
 		flags   { "Optimize" }
 
-    project "ice"
+	project "ice"
 		kind "ConsoleApp"
 		language "C++"
         files { "src/main.cpp", "src/engine/**.h", "src/engine/**.cpp", "src/game/**.h", "src/game/**.cpp" }
 		includedirs { "src/" }
 
-        configuration "windows"
-            -- TODO: Remove hard-coded paths
-            libdirs     { "C:/codelibs/pdcurses/lib/" }
-            includedirs { "C:/codelibs/pdcurses/include/" }
-            links       { "pdcurses" }
+		configuration "windows"
+			-- TODO: Remove hard-coded paths
+			libdirs     { "C:/codelibs/pdcurses/lib/" }
+			includedirs { "C:/codelibs/pdcurses/include/" }
+			links       { "pdcurses" }
 			defines     { "WINDOWS" }
 
-        configuration "linux"
-            links { "ncurses" }
+		configuration "linux"
+			links { "ncurses" }
 			libdirs { "/usr/local/lib" }
 			flags { "ExtraWarnings", "FatalWarnings" }
 
