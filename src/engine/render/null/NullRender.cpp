2d38626a8b92fee2a0c257bba13df8620dbe9766
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2013-08-24 14:16:00 +0300

    Fixed the other printf... bit spammy commits but it happens

--- src/engine/render/null/NullRender.cpp
+++ src/engine/render/null/NullRender.cpp
@@ -6,7 +6,7 @@ namespace null {
 
 	void NullRender::update(){
 		printf("Windows visible %lu \n", uiSystem->windows.size() );
-		printf("Actors in room 0,0,0: %i \n", sceneSystem->getActorManager()->getActorsInRoom(vec3(0,0,0)).size() );
+		printf("Actors in room 0,0,0: %lu \n", sceneSystem->getActorManager()->getActorsInRoom(vec3(0,0,0)).size() );
 		printf("---\n");
 
 		getchar();
