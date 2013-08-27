3fbfade2e87faa0d0d868f0725c2b6602777fd6e
Shammah <contact@protoman.net>
2013-08-27 23:09:47 +0200

    Converted spaces to tabs.

--- src/engine/math/vec2.cpp
+++ src/engine/math/vec2.cpp
@@ -3,94 +3,94 @@
 #include "engine/math/vec2.h"
 
 vec2::vec2() {
-    x = 0;
-    y = 0;
+	x = 0;
+	y = 0;
 }
 
 vec2::vec2(const int newX, const int newY) {
-    x = newX;
-    y = newY;
+	x = newX;
+	y = newY;
 }
 
 vec2::vec2(const vec2& vec) {
-    x = vec.x;
-    y = vec.y;
+	x = vec.x;
+	y = vec.y;
 }
 
 vec2& vec2::operator+=(const vec2 &rhs) {
-    this->x += rhs.x;
-    this->y += rhs.y;
-    return *this;
+	this->x += rhs.x;
+	this->y += rhs.y;
+	return *this;
 }
 
 vec2& vec2::operator-=(const vec2 &rhs) {
-    this->x -= rhs.x;
-    this->y -= rhs.y;
-    return *this;
+	this->x -= rhs.x;
+	this->y -= rhs.y;
+	return *this;
 }
 
 vec2& vec2::operator*=(int rhs) {
-    this->x *= rhs;
-    this->y *= rhs;
-    return *this;
+	this->x *= rhs;
+	this->y *= rhs;
+	return *this;
 }
 
 vec2& vec2::operator/=(int rhs) {
-    this->x /= rhs;
-    this->y /= rhs;
-    return *this;
+	this->x /= rhs;
+	this->y /= rhs;
+	return *this;
 }
 
 bool vec2::operator==(const vec2 &rhs) {
-    return this->x == rhs.x && this->y == rhs.y;
+	return this->x == rhs.x && this->y == rhs.y;
 }
 
 bool vec2::operator!=(const vec2 &rhs) {
-    return this->x != rhs.x || this->y != rhs.y;
+	return this->x != rhs.x || this->y != rhs.y;
 }
 
 vec2 vec2::operator+(const vec2 &rhs) {
-    return vec2(*this) += rhs;
+	return vec2(*this) += rhs;
 }
 
 vec2 vec2::operator-(const vec2 &rhs) {
-    return vec2(*this) -= rhs;
+	return vec2(*this) -= rhs;
 }
 
 vec2 vec2::operator*(int rhs) {
-    return vec2(*this) *= rhs;
+	return vec2(*this) *= rhs;
 }
 
 vec2 vec2::operator/(int rhs) {
-    return vec2(*this) /= rhs;
+	return vec2(*this) /= rhs;
 }
 
 int vec2::operator*(const vec2 &rhs) {
-    return this->x * rhs.x + this->y * rhs.y;
+	return this->x * rhs.x + this->y * rhs.y;
 }
 
 int vec2::distanceSquared() const {
-    return x * x + y * y;
+	return x * x + y * y;
 }
 
 double vec2::distance() const {
-    return sqrt((double) distanceSquared());
+	return sqrt((double) distanceSquared());
 }
 
 bool vec2::operator < (const vec2 & rhs) const {
-    if ( distanceSquared() < rhs.distanceSquared() ) {
-        return true;
-    }    
-    else {
-        return false;
-    }
+	if ( distanceSquared() < rhs.distanceSquared() ) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 bool vec2::operator > (const vec2 & rhs) const {
-    if ( distanceSquared() > rhs.distanceSquared() ) {
-        return true;
-    }    
-    else {
-        return false;
-    }
+	if ( distanceSquared() > rhs.distanceSquared() ) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
