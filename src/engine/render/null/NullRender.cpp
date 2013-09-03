27899de3d06fe0e07843321b587ba025974b5542
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2013-09-03 16:27:15 +0300

    Fixed some define settings in the nullrender

--- src/engine/render/null/NullRender.cpp
+++ src/engine/render/null/NullRender.cpp
@@ -17,15 +17,17 @@ namespace null {
 			uiSystem->windows.size(); //these are not unused
 			sceneSystem->getActorManager();
 
-#ifdef NULLRENDER_HISTORY
-			console::ConsoleSystem::MsgHistory hist = console::ConsoleSystem::getMsgHistory();
+			if(NULLRENDER_HISTORY == true){
+				console::ConsoleSystem::MsgHistory hist = console::ConsoleSystem::getMsgHistory();
 
-			system("cls");
+				if(NULLRENDER_CLS == true){
+					system("cls");
+				}
 
-			for(unsigned int i = 0; i < hist.size(); ++i){
-				printf("%s\n",hist[i].c_str() );
+				for(unsigned int i = 0; i < hist.size(); ++i){
+					printf("%s\n",hist[i].c_str() );
+				}
 			}
-#endif
 
 			//printf("Windows visible %ui \n", uiSystem->windows.size() );
 			//printf("Actors in room 0,0,0: %ui \n", sceneSystem->getActorManager()->getActorsInRoom(vec3(0,0,0)).size() );
@@ -34,7 +36,6 @@ namespace null {
 			stepsLeft = NULLRENDER_STEP;
 
 			if(NULLRENDER_PAUSE == true){
-#pragma warning(suppress: 6031) //this is only to pause the render, works as expected
 				getchar();
 			}
 		}
