0d27703dd04dc9faebc7ec92a1c7ad79fc730c53
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2013-11-02 22:06:09 +0200

    Moved methods to separate .cpp file for ActorStorage

---
+++ src/engine/actor/ActorStorage.cpp
@@ -0,0 +1,55 @@
+#include "engine/actor/ActorStorage.h"
+
+namespace engine {
+namespace actor {
+
+	void ActorStorage::removeActor(ActorBase* actor){
+		for(unsigned int i = 0;i < actors.size();++i){
+			if(actors.at(i) == actor){
+				actors.erase( actors.begin() + i);
+				LOG_INFO("actor removed!");
+			}
+		}
+	}
+
+	/**
+	 * Insert actor to the manager.
+	 * @param actor actor to be inserted
+	 */
+	void ActorStorage::insertActorToRoom(ActorBase* actor){
+		actors.push_back(actor);
+	}
+
+	/**
+	 * Adds actor to the remove queue.
+	 *
+	 * @param actor pointer to the actor to be removed.
+	 */
+	void ActorStorage::deleteActor(ActorBase* actor) {
+		removeQueue.push_back(actor);
+	}
+
+	/**
+	 * Remove actors queue for remove
+	 */
+	void ActorStorage::handleRemoveQueue() {
+		for(unsigned int i = 0;i < removeQueue.size();++i){
+			removeActor(removeQueue.at(i));
+		}
+	}
+
+	std::vector<ActorBase *> ActorStorage::getActorsInRoom(const vec3 location){
+		std::vector<ActorBase *> output;
+
+		for(unsigned int i = 0;i < actors.size();++i){
+			vec3 loc = actors.at(i)->getLocation();
+			if(loc == location){
+				output.push_back(actors.at(i));
+			}
+		}
+
+		return output;
+	}
+
+}
+}
--- src/engine/actor/ActorStorage.h
+++ src/engine/actor/ActorStorage.h
@@ -25,54 +25,15 @@ namespace actor {
 
 			void moveActor(actor::ActorBase * actor,vec2 dir);
 
-			void removeActor(ActorBase* actor){
-				for(unsigned int i = 0;i < actors.size();++i){
-					if(actors.at(i) == actor){
-						actors.erase( actors.begin() + i);
-						LOG_INFO("actor removed!");
-					}
-				}
-			}
+			void removeActor(ActorBase* actor);
 
 		public:
-			/**
-			 * Insert actor to the manager.
-			 * @param actor actor to be inserted
-			 */
-			void insertActorToRoom(ActorBase* actor){
-				actors.push_back(actor);
-			}
 
-			/**
-			 * Adds actor to the remove queue.
-			 *
-			 * @param actor pointer to the actor to be removed.
-			 */
-			void deleteActor(ActorBase* actor) {
-				removeQueue.push_back(actor);
-			}
+			void insertActorToRoom(ActorBase* actor);
+			void deleteActor(ActorBase* actor);
+			void handleRemoveQueue();
+			std::vector<ActorBase *> getActorsInRoom(const vec3 location);
 
-			/**
-			 * Remove actors queue for remove
-			 */
-			void handleRemoveQueue() {
-				for(unsigned int i = 0;i < removeQueue.size();++i){
-					removeActor(removeQueue.at(i));
-				}
-			}
-
-			std::vector<ActorBase *> getActorsInRoom(const vec3 location){
-				std::vector<ActorBase *> output;
-
-				for(unsigned int i = 0;i < actors.size();++i){
-					vec3 loc = actors.at(i)->getLocation();
-					if(loc == location){
-						output.push_back(actors.at(i));
-					}
-				}
-
-				return output;
-			}
 	};
 
 }
