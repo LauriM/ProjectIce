a977e3947ca5f71254f116d905e68f88040d8b73
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2013-11-04 17:10:54 +0200

    Visual enhancements to the vec2 class.

--- src/engine/math/vec2.cpp
+++ src/engine/math/vec2.cpp
@@ -4,17 +4,20 @@
 
 #include "engine/math/random.h"
 
-vec2::vec2() : x(0), y(0) {
+vec2::vec2()
+	: x(0)
+	, y(0)
+{ }
 
-}
+vec2::vec2(const int newX, const int newY)
+	: x(newX)
+	, y(newY)
+{ }
 
-vec2::vec2(const int newX, const int newY) : x(newX), y(newY) {
-
-}
-
-vec2::vec2(const vec2& vec) : x(vec.x), y(vec.y) {
-
-}
+vec2::vec2(const vec2& vec)
+	: x(vec.x)
+	, y(vec.y)
+{ }
 
 vec2& vec2::operator +=(const vec2 &rhs) {
 	this->x += rhs.x;
